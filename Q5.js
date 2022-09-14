671819cda5cba8a643556b256166ef714b4362bd
mydreamis-a <9104141@gmail.com>
2022-09-14 10:31:17 +0900

    정답 코드 첨부

--- Q5.js
+++ Q5.js
@@ -1,13 +1,25 @@
 const { log } = console;
 //
 // ㅜ 배열의 모든 순열을 출력하는 함수 (재귀 함수 사용)
-function swap(strArr, idx1, idx2) {
-  //
+function swap(strArr, index1, index2) {
+  let temp = strArr[index1];
+  strArr[index1] = strArr[index2];
+  strArr[index2] = temp;
 }
 function permute(strArr, begin, end) {
-  //
+  if (begin === end) {
+    console.log(strArr);
+  } else {
+    for (let i = begin; i < end + 1; i++) {
+      swap(strArr, begin, i);
+      permute(strArr, begin + 1, end);
+      swap(strArr, begin, i);
+    }
+  }
 }
 function permuteArray(strArr) {
-  //
+  permute(strArr, 0, strArr.length - 1);
 }
 permuteArray(["1", "2", "3"]);
+//
+// 정답 코드

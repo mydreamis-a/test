152af6412cfae679a40d0a31f2a1fa88b000379a
mydreamis-a <9104141@gmail.com>
2022-09-14 10:02:12 +0900

    회문 완료

--- Q2.js
+++ Q2.js
@@ -3,20 +3,32 @@ const { log } = console;
 // ㅜ 소수 여부를 판단하는 함수
 // ㅜ 소수란 1보다 큰 자연수중 1과 자신만을 약수로 가지는 수를 말한다.
 // ㅜ 주어진 함수 내부를 구현하고 결과에 따라 true 또는 false를 리턴한다.
-function isPrime(n) {
+function primeFactor(n) {
   //
   const arr = new Array();
   //
-  for (let i = 1; i <= n; i++) {
+  for (let i = 2; i < n; i++) {
     //
     if (!(n % i)) {
       //
-      arr.push(i);
+      let decimal = true;
+      //
+      for (let j = 2; j < i; j++) {
+        //
+        if (!(i % j)) {
+          //
+          decimal = false;
+          break;
+        }
+      }
+      if (decimal === true) {
+        //
+        arr.push(i);
+      }
     }
   }
-  return arr.length === 2 ? true : false;
+  return arr.length === 0 ? true : false;
 }
-log(isPrime(31));
-log(isPrime(10));
+log(primeFactor(13));
 //
-// 09.13.14 수정
+// 09.14.09 수정
--- Q6.js
+++ Q6.js
@@ -3,10 +3,30 @@ const { log } = console;
 // ㅜ 회문 여부를 판단하는 함수 (재귀 함수 사용)
 // ㅜ 주어진 함수 내부를 구현하고 결과에 따라 true 또는 false를 리턴한다.
 function isPalindromeRecursive(word) {
-    //
+  //
+  const beginPos = 0;
+  word = word.split("");
+  const endPos = word.length - 1;
+  //
+  return isPalindromeRecursiveHelper(word, beginPos, endPos);
 }
 function isPalindromeRecursiveHelper(word, beginPos, endPos) {
+  //
+  if (word[beginPos] === word[endPos]) {
     //
+    if (beginPos >= endPos / 2) {
+      //
+      return true;
+    }
+    beginPos = beginPos + 1;
+    endPos = endPos - 1;
+    //
+    return isPalindromeRecursiveHelper(word, beginPos, endPos);
+  }
+  //
+  else return false;
 }
 log(isPalindromeRecursive("appleelppa"));
 log(isPalindromeRecursive("appleelpaa"));
+//
+// 09.14.09 수정
